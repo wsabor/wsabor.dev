601a6cb2a80334ee2452b2c8cf027d53cbad8c1e
Wagner Sabor <wsaborjr@gmail.com>
2025-09-07 12:07:05 -0300

    chore(new-post): edit post Hackaton Agrotech 2025

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -33,7 +33,7 @@ export function ImageGallery({ images }: ImageGalleryProps) {
 
   // 3. Prepara a lista de slides para o lightbox
   const slides = images.map((item) => ({
-    src: `/img/projects/${item.image}`,
+    src: `/img/${item.image}`,
     title: item.caption,
   }));
 
