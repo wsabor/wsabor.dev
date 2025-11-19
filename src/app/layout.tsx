4a560b6489a07bcfc720ff337baba8cfcc0bd727
Wagner Sabor <wsaborjr@gmail.com>
2025-11-19 10:13:59 -0300

    chore: Vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import "./globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -123,6 +124,7 @@ export default function RootLayout({
           </main>
           <Footer />
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
